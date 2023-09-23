8e5069c1070a3df2983e38cd9f67b09aa0a438f4
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-09-23 20:42:55 +0900

    add: エラー対処 カテゴリー入れ

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -2,7 +2,7 @@ import { UserInfoIcons } from '../../components/portfolio/UserHeadLine';
 import { GridSkillsSection } from '../../components/portfolio/UserSkills';
 import { GridIntroduceSection } from '../../components/portfolio/UserIntroduce';
 import { ArticleCard } from '../../components/portfolio/ArticleCard';
-import { GitHubContibutions } from "../../components/portfolio/github.tsx";
+import { GitHubContibutions } from '../../components/portfolio/github';
 
 export default function Home() {
   return (
